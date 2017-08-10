b5a62e1ca6bb81a2d6fe6b4ef23397547228ba63
sourabhdpunja <punja.so@husky.neu.edu>
2017-08-09 20:10:12 -0400

    Commit Assignment 5

--- assignment/models/database.js
+++ assignment/models/database.js
@@ -5,13 +5,14 @@
 var q = require('q');
 
 var connectionString = 'mongodb://127.0.0.1:27017/webdev_summer2_2017'; // for local
-// if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-//     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-//     var password = process.env.MLAB_PASSWORD_WEBDEV;
-//     connectionString = 'mongodb://' + username + ':' + password;
-//     connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
-// }
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds129459.mlab.com:29459/heroku_4kxrfc6l'; // user yours
+}
 // Replace "@ds157268.mlab.com:57268/heroku_nh37fqq4"
+// mongodb://<dbuser>:<dbpassword>@ds129459.mlab.com:29459/heroku_4kxrfc6l
 // above with your own URL given to you by mLab
 
 var mongoose = require("mongoose");
