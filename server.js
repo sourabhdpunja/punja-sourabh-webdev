c627f98ad1dff1320bc5723a0f4f1efa392db69e
sourabhdpunja <punja.so@husky.neu.edu>
2017-08-18 07:23:25 -0400

    added home page

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var app = require('./express');
 var express = app.express;
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+app.use(session({
+    secret: 'this is the secret',
+    resave: true,
+    saveUninitialized: true
+}));
 
 app.use(express.static(__dirname + '/public'));
 
